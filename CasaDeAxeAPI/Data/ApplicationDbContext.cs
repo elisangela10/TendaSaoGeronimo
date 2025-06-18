66c3289ceabf521277165023a97daac838793a17
Elisangela Miranda dos Anjos <50150279+elisangela10@users.noreply.github.com>
2025-06-18 00:51:47 -0300

    Projeto base de user

---
+++ CasaDeAxeAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CasaDeAxeAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDeAxeAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Gira> Giras { get; set; }
+        public DbSet<TextoPonto> TextoPonto { get; set; }
+    }
+}
